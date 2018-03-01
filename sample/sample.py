4c1d857df71eeb131b0427ff31c48414589bb9cc
AlexSmith <hoshialisato@gmail.com>
2018-03-01 16:40:36 +0000

    added a function outline

--- sample/sample.py
+++ sample/sample.py
@@ -3,9 +3,24 @@ import sys
 
 # data import/export (fionn)
 def parse_data(filename):
-    #file = open(filename)
+    with open(filename, "r") as f:
+        file_rows = [line.rstrip("\n") for line in f]
 
+        attributes = file_rows[0].split(" ")
 
+        num_rows = attributes[0]
+        num_cols = attributes[1]
+        ing_per_slice = attributes[2]
+        cells_per_slice = attributes[3]
+        rows = file_rows[1:]
+
+        return {
+            "num_rows": num_rows,
+            "num_cols": num_cols,
+            "ing_per_slice": ing_per_slice,
+            "cells_per_slice": cells_per_slice,
+            "rows": rows
+        }
 
 # slide area, etc (jack)
 
@@ -18,4 +33,4 @@ def is_valid_slice(sliceSizeData,ruleData,ingData):
 
 # RUN FILE
 
-data = parse_data("")
+data = parse_data("example.in")
