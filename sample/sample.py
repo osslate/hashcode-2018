b55e168f1acd6d2c1c18ba89194e5c3519f85a0a
AlexSmith <hoshialisato@gmail.com>
2018-03-01 16:38:56 +0000

    start of function

--- sample/sample.py
+++ sample/sample.py
@@ -10,6 +10,9 @@ def parse_data(filename):
 # slide area, etc (jack)
 
 # can be used (alex)
+def is_valid_slice(sliceSizeData,ruleData,ingData):
+    # checking ingredients in the slice row
+    print('this is here so the outlin of this function dose not break everything')
 
 # scoring (bradley)
 
