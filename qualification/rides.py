2a5c28aa1b2843a072c42835c4c404528ab8489e
AlexSmith <hoshialisato@gmail.com>
2018-03-01 18:49:52 +0000

    init

--- qualification/rides.py
+++ qualification/rides.py
@@ -7,7 +7,6 @@ def parse_data(filename):
     data_lines = [line.rstrip("\n") for line in data_file]
 
     attributes = data_lines[0].split(" ")
-
     return {
         "num_rows": attributes[0],
         "num_cols": attributes[1],
@@ -18,6 +17,28 @@ def parse_data(filename):
         "rows": data_lines[1:]
     }
 
+class Car:
+
+    def __init__(self, id):
+        self.id = id
+        self.free = False
+        self.col = 0
+        self.row = 0
+        self.rides = []
+
+class Carpool:
+
+    def __init__(self):
+        self.vehicles = [Car(i) for i in range(0,parsed['num_vehicles'])]
+
+
+class Passengerpool:
+    
+    def __init__(self):
+        print('aaaaa')
 # run
 
 parsed = parse_data("a_example.in")
+
+x = Carpool()
+print(x.vehicles)
