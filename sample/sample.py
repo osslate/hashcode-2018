8702058fbb69fdc19b48baed31c788dcd5c2c94b
Fionn Kelleher <fionnkell@gmail.com>
2018-03-01 04:19:45 +0000

    Add sample.py

---
+++ sample/sample.py
@@ -0,0 +1,18 @@
+# imports
+import sys
+
+# data import/export (fionn)
+def parse_data(filename):
+    #file = open(filename)
+
+
+
+# slide area, etc (jack)
+
+# can be used (alex)
+
+# scoring (bradley)
+
+# RUN FILE
+
+data = parse_data("")
